99b3e1e5c4e71bccd7fdb77fb05b4efd7292bf01
thierry-silbermann <thierry.silbermann@gmail.com>
2025-03-09 17:14:52 +0000

    check_col_row.c

--- rush01/create_grid.c
+++ rush01/create_grid.c
@@ -1,25 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_grid.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilberm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/09 11:54:40 by tsilberm          #+#    #+#             */
+/*   Updated: 2025/03/09 14:52:47 by tsilberm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-int     **ft_allocate_mem_tab(int size)
+int	**ft_allocate_mem_tab(int size)
 {
-        int     i;
-        int     j;
-        int     **tab;
+	int	i;
+	int	j;
+	int	**tab;
 
-        i = 0;
-        tab = malloc(size * sizeof(int *));
-        while (i < size)
-        {
-                tab[i] = malloc(size * sizeof(int));
-                j = 0;
-                while (j < size)
-                {
-                        tab[i][j] = 0;
-                        j++;
-                }
-                i++;
-        }
-        return (tab);
+	i = 0;
+	tab = malloc(size * sizeof(int *));
+	while (i < size)
+	{
+		tab[i] = malloc(size * sizeof(int));
+		j = 0;
+		while (j < size)
+		{
+			tab[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (tab);
 }
 
 int	*ft_allocate_mem_arr(int size)
@@ -37,20 +49,20 @@ int	*ft_allocate_mem_arr(int size)
 	return (arr);
 }
 
-void    ft_free_tab(int **tab, int size)
+void	ft_free_tab(int **tab, int size)
 {
-        int     i;
+	int	i;
 
-        i = 0;
-        while(i < size) {
-                free(tab[i]);
-                i++;
-        }
-        free(tab);
+	i = 0;
+	while (i < size)
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
 }
 
-void ft_free_arr(int *arr)
+void	ft_free_arr(int *arr)
 {
 	free(arr);
 }
-
--- rush01/parse_input.c
+++ rush01/parse_input.c
@@ -1,26 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilberm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/09 11:55:58 by tsilberm          #+#    #+#             */
+/*   Updated: 2025/03/09 14:07:12 by tsilberm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <stdio.h>
 
-int     *ft_allocate_mem_arr(int size);
+int	*ft_allocate_mem_arr(int size);
 
 /*
 ./rush-01 "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2" | cat -e
 */
 
-int	get_N(char *str)
+int	get_n(char *str)
 {
 	int	i;
-	int	N;
+	int	size;
 
 	i = 0;
-	N = 0;
+	size = 0;
 	while (str[i])
 	{
-		if ((str[i] - '0') > N)
-			N = str[i] - '0';
+		if ((str[i] - '0') > size)
+			size = str[i] - '0';
 		i++;
 	}
-	return (N);
+	return (size);
 }
 
 int	*arr_input(char *str, int size)
@@ -34,7 +45,7 @@ int	*arr_input(char *str, int size)
 	arr = ft_allocate_mem_arr(size * 4);
 	while (str[i])
 	{
-		if (str[i] >= '0' && str[i] <= '9')
+		if (str[i] >= '1' && str[i] <= '9')
 		{
 			arr[j] = str[i] - '0';
 			j++;
@@ -43,30 +54,36 @@ int	*arr_input(char *str, int size)
 	}
 	return (arr);
 }
-
-void    display_all_view(int *all_view, int N)
+/*
+// This function will need to be removed
+void	display_all_view(int *all_view, int N)
 {
-        int     i;
+	int	i;
 
-        i = 0;
-        while (i < 4 * N)
-        {
-                printf("%d ", all_view[i]);
-                i++;
-        }
-        printf("\n");
+	i = 0;
+	while (i < 4 * N)
+	{
+		printf("%d ", all_view[i]);
+		i++;
+	}
+	printf("\n");
 }
-
 
 int	main(int argc, char *argv[])
 {
 	int	*input;
-	int	N;
+	int	size;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (0);
+	}
 	printf("%d\n", argc);
 	printf("%s\n", argv[1]);
-	N = get_N(argv[1]);
-	printf("N: %d\n", N);
-	input = arr_input(argv[1], N);
-	display_all_view(input, N);
-}
+	size = get_n(argv[1]);
+	printf("N: %d\n", size);
+	input = arr_input(argv[1], size);
+	display_all_view(input, size);
+	// Need to free input
+}*/
